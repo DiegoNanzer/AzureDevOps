c759909c4d929050e5fe9361888992ac3638b7c5
DiegoNanzer <diego_nanzerdiego_nanzer@yahoo.com.br>
2022-04-19 20:12:05 -0300

    Add Smurf

---
+++ AzureDevOps.Domain/Smurf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDevOps.Domain
+{
+    public class Smurf
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Campo deve ter no mínimo 3 caracteres.")]
+        public string Name { get; set; }
+
+        public int Idade { get; set; } = 0;
+    }
+}
