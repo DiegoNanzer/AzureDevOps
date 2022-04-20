9cd31192576a6980700fbc61b35e37922c58f81c
DiegoNanzer <diego_nanzerdiego_nanzer@yahoo.com.br>
2022-04-19 21:59:43 -0300

    add context and migration

---
+++ AzureDevOps.Infra/Migrations/20220420005814_ChangeTableName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzureDevOps.Infra.Migrations
+{
+    public partial class ChangeTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Smurves",
+                table: "Smurves");
+
+            migrationBuilder.RenameTable(
+                name: "Smurves",
+                newName: "Smurfs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Smurfs",
+                table: "Smurfs",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Smurfs",
+                table: "Smurfs");
+
+            migrationBuilder.RenameTable(
+                name: "Smurfs",
+                newName: "Smurves");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Smurves",
+                table: "Smurves",
+                column: "Id");
+        }
+    }
+}
